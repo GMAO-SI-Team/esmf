83db5389b28c2cada7c9ecb10cb81364615aba83
Matthew Thompson <matthew.thompson@nasa.gov>
2024-01-16 10:38:57 -0500

    Try the free in reverse order

--- src/Infrastructure/VM/src/ESMCI_VMKernel.C
+++ src/Infrastructure/VM/src/ESMCI_VMKernel.C
@@ -784,9 +784,6 @@ void VMK::finalize(int finalizeMpi){
   delete [] cid;
   delete [] ssiLocalPetList;
   delete [] ssiLocalDevList;
-  for (auto i=0; i<(signed)customType.size(); i++){
-    MPI_Type_free(&(customType[i]));
-  }
 #ifdef ESMF_NVML
   // Have access to NVIDIA management library (NVML)
   nvmlShutdown();  // shut down NVML
@@ -800,6 +797,12 @@ void VMK::finalize(int finalizeMpi){
     MPI_Comm_free(&mpi_c_ssi);
 #endif
     if (finalizeMpi)
+      // We need to now free the custom MPI data types in the
+      // vector<customType> customType
+      // in reverse order of their creation
+      for (auto i=(signed)customType.size()-1; i>=0; i--){
+         MPI_Type_free(&(customType[i]));
+      }
       MPI_Finalize();
   }
 }
